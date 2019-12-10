9decec558e64545a1ac027ff2f43719454dfb0e1
eswanthdev <53303165+eswanthdev@users.noreply.github.com>
2019-12-10 12:15:21 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_security_group" "default" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/8"]
+    cidr_blocks = ["10.0.0.0/12"]
   }
   
   ingress {
